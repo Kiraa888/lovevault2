fb861e063a0799a1f20623d4103d302786956585
Animesh Bauri <159995350+Kiraa888@users.noreply.github.com>
2026-09-18 16:36:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Love Vault - Service Worker
 // Version 1.1
 
-const CACHE_NAME = 'love-vault-v4.1';
+const CACHE_NAME = 'love-vault-v5';
 const RUNTIME_CACHE = 'love-vault-runtime';
 
 // Files to cache immediately on install
